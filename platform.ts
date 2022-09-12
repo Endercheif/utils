18c9eba4afd76693d5b3d4afa3f48baf8522deb3
Endercheif <reet22singh@gmail.com>
2022-09-12 11:29:45 -0700

    feat: types for IsNode & IsDeno

--- platform.ts
+++ platform.ts
@@ -1,8 +1,16 @@
-export function isNode(): boolean {
+type _node = { process: unknown };
+export type IsNode = typeof globalThis extends _node ? true : false;
+
+type _deno = { Deno: unknown };
+export type IsDeno = typeof globalThis extends _deno ? true : false;
+
+export function isNode(): IsNode {
+  // @ts-ignore: works in Node
   return "process" in globalThis && "global" in globalThis;
 }
 
 export function isDeno(): boolean {
+  // @ts-ignore: works in Deno
   return "Deno" in globalThis;
 }
 
